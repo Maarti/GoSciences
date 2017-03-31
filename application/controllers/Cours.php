318ee6c52da565c31d5fa849c3dd14dd3cfc7298
Maarti <bryan.martinet@gmail.com>
2017-03-31 19:39:35 +0200

    SEO Cours

--- application/controllers/Cours.php
+++ application/controllers/Cours.php
@@ -5,6 +5,8 @@ class Cours extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        $this->data['tab_title'] = 'GoSciences - Aide scolaire à Orléans et ses environs | Cours';
+        $this->data['meta_desc'] = 'GoSciences propose de l\'aide scolaire de qualité dans les matières scientifiques à Orléans, La Ferté-Saint-Aubin, La Chapelle-Saint-Mesmin, Saint-Jean-de-Braye, Saint-Jean-le-Blanc, Saint-Jean-de-la-Ruelle, Olivet, Saran, Lamotte-Beuvron, Vouzon, Marcilly-en-Villette, Menestreau-en-Villette, Saint-Cyr-en-Val, Ligny-le-Ribault, Jouy-le-Potier.';
         //$this->output->enable_profiler(true);
     }
     
@@ -14,17 +16,18 @@ class Cours extends CI_Controller {
 
     public function tarifs($id_prest = null){
         $this->load->model('prestation_model'); 
-        $data['tarifs'] = $this->classe_model->get_tarifs($id_prest);
+        $this->data['tarifs'] = $this->classe_model->get_tarifs($id_prest);
         $libelle_prest = $this->prestation_model->read('libelle',array('id'=>$id_prest))->row();
-        if (empty($data['tarifs']) || empty($libelle_prest))
+        if (empty($this->data['tarifs']) || empty($libelle_prest))
             redirect('site/accueil', 'refresh');
         else{
-            $data['tab_title'] = 'GoSciences - Tarifs';
-            $data['id_prest'] = $id_prest;
-            $data['page_title'] = 'Tarifs '.$libelle_prest->libelle;
-            $this->load->view('site/header', $data);
-            $this->load->view('site/menu', $data);
-            $this->load->view('cours/tarifs', $data);
+            $this->data['tab_title'] = 'GoSciences - Aide scolaire à Orléans et ses environs | Tarifs '.$libelle_prest->libelle;
+            $this->data['meta_desc'] = 'Découvrez les tarifs "'.$libelle_prest->libelle.'" proposés par GoSciences en Mathématiques, SVT, Physique et Chimie pour les différentes classes du collège et du lycée à Orléans, La Ferté-Saint-Aubin, La Chapelle-Saint-Mesmin, Saint-Jean-de-Braye, Saint-Jean-le-Blanc, Saint-Jean-de-la-Ruelle, Olivet, Saran, Lamotte-Beuvron, Vouzon, Marcilly-en-Villette, Menestreau-en-Villette, Saint-Cyr-en-Val, Ligny-le-Ribault, Jouy-le-Potier.';
+            $this->data['id_prest'] = $id_prest;
+            $this->data['page_title'] = 'Tarifs '.$libelle_prest->libelle;
+            $this->load->view('site/header', $this->data);
+            $this->load->view('site/menu', $this->data);
+            $this->load->view('cours/tarifs', $this->data);
             $this->load->view('site/footer');
         }
     }
