9f97223f8f1d02f17a269267ee99cb9ec7cbeea7
Maarti <maarti@ASUS-MAARTI.lan>
2017-07-30 17:08:43 +0200

    ajout vignette reussite bac 2017

--- application/views/site/accueil.php
+++ application/views/site/accueil.php
@@ -4,27 +4,32 @@
 
         <? $this->load->view('include/homepagetitle'); ?>
 
-        <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+        <div class="orbit" role="region" aria-label="GoSciences Accueil" data-orbit>
         <ul class="orbit-container">
           <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
           <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-          <li class="is-active orbit-slide">
+		<li class="is-active orbit-slide">
+            <img class="orbit-image" src="<?=img_url('reussite-examens-2017-opti.png')?>" alt="100% de réussite aux examens nationnaux en 2017 !">
+            <figcaption class="orbit-caption">Toute l'équipe de GoSciences félicite et est fière d'avoir pu contribuer à la réussite de tous ses élèves !</figcaption>
+          </li>
+          <li class="orbit-slide">
             <img class="orbit-image" src="<?=img_url('eleve-tableau-opti.jpeg')?>" alt="Élève tableau">
             <figcaption class="orbit-caption">L'art de la réussite consiste à savoir s'entourer des meilleurs</figcaption>
           </li>
-          <li class="is-active orbit-slide">
+          <li class="orbit-slide">
             <img class="orbit-image" data-open="modalRepDuCentre" src="<?=img_url('rep_centre_20170406_thumb-opti.png')?>" alt="Article République du Centre">
           <figcaption class="orbit-caption" data-open="modalRepDuCentre">GoSciences interviewé par la République du Centre à propos du soutien scolaire (voir l'article)</figcaption>
           </li>
-          <li class="orbit-slide">
+          <!--<li class="orbit-slide">
             <img class="orbit-image" src="<?=img_url('remise-prix-opti.jpeg')?>" alt="Photo remise de prix">
             <figcaption class="orbit-caption">Prix de la Fondation de l'Université d'Orléans niveau Master remis à Sulayman Benmerzoug</figcaption>
-          </li>          
+          </li>     -->     
         </ul>
         <nav class="orbit-bullets">
-          <button class="is-active" data-slide="0"><span class="show-for-sr">L'art de la réussite consiste à savoir s'entourer des meilleurs</span><span class="show-for-sr">Current Slide</span></button>
-          <button data-slide="1"><span class="show-for-sr">GoSciences interviewé par la République du Centre à propos du soutien scolaire (voir l'article)</span></button>
-          <button data-slide="2"><span class="show-for-sr">Prix de la Fondation de l'Université d'Orléans niveau Master remis à Sulayman Benmerzoug</span></button>
+	<button class="is-active" data-slide="0"><span class="show-for-sr">Toute l'équipe de GoSciences félicite et est fière d'avoir pu contribuer à la réussite de tous ses élèves !</span><span class="show-for-sr">Current Slide</span></button>
+          <button data-slide="1"><span class="show-for-sr">L'art de la réussite consiste à savoir s'entourer des meilleurs</span></button>
+          <button data-slide="2"><span class="show-for-sr">GoSciences interviewé par la République du Centre à propos du soutien scolaire (voir l'article)</span></button>
+<!--          <button data-slide="2"><span class="show-for-sr">Prix de la Fondation de l'Université d'Orléans niveau Master remis à Sulayman Benmerzoug</span></button>-->
         </nav>
       </div>
         
